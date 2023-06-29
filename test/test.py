005597868112434b48e110551b2d6c11d0bde173
Joel <jojo_0713@live.ca>
2023-06-29 16:12:02 -0400

    init first pre-commit

--- test/test.py
+++ test/test.py
@@ -1,1 +1,8 @@
-## To Implement
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(2 + 2, 4)
+
+if __name__ == '__main__':
+    unittest.main()
