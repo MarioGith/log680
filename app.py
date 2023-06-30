8cec959e56e0fcce82f7046db4338b7db5c9f381
Wood-Computer <kevin.duong.1@ens.etsmtl.ca>
2023-06-30 14:17:49 -0400

    Test commit pre-commit

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from api.routes import register_routes
 app = Flask(__name__)
 api = Api(app)
 
-# Initialize Swagger
+
+# Initialize Swagger!
 swagger = Swagger(app)
 
 # Register routes
 register_routes(api)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
