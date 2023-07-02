b3b53e1b211544c9a0735e794e1818e0c45da9d3
Pato <poncepatricio1@gmail.com>
2023-07-01 22:36:42 -0400

    removed comment to trigger PR build

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ import os
 import requests
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 
-#ajout d'un commentaire pour lancer le build
 
 class Main:
     def __init__(self):
